769cd10d12092b1d2af51d022c6ea8e329531932
irina <eiv2212@gmail.com>
2022-12-15 16:36:53 +0300

    задача 3

---
+++ Task_3.py
@@ -0,0 +1,22 @@
+# 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт
+# разницу между максимальным и минимальным значением дробной части элементов.
+
+# *Пример:*
+
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19 (максимальное значение у числа 1.2 ,
+# минимальное у 10.01)
+
+def separate_fraction(num: float) -> float:
+    '''Отделяет дробную часть от целой'''
+    list_num = str(num).split('.') #['1','11']
+    return float('0.' + list_num[1])
+
+def max_vs_min_fraction(num_list: list[float]) -> float:
+    '''Возвращает разницу между max и min значением списка'''
+    new_list = [separate_fraction(i) for i in num_list if int(i) != float(i)]
+    print(new_list)
+    return max(new_list) - min(new_list)
+
+n = [1.1, 1.2, 3.1, 5, 10.01]
+
+print(max_vs_min_fraction(n))
